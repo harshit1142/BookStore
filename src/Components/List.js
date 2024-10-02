a655fbcd992551a3791b50454848e105d4a03e96
PALASH2201 <palashmahajan22@gmail.com>
2024-10-02 17:50:51 +0530

    Changed the UI

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,22 +1,39 @@
-import React from 'react'
-import { removeFav } from '../redux/actions/Fav'
-import { connect } from 'react-redux'
- function List({title,author,link,Fav,ele}) {
+import React from "react";
+import { removeFav } from "../redux/actions/Fav";
+import { connect } from "react-redux";
+import styles from "./favbook.module.css";
+import { Book, Trash2 } from "lucide-react";
+function List({ title, author, link, Fav, ele }) {
   return (
-    <div className='list'>
-        <h1>{title}</h1>
-        <p>by-{author}</p>
-        <a href={link} target="_blank" rel="noopener noreferrer" > <button className='btn'>Read📖</button></a>
-        <button className='btn btn-back' onClick={()=>Fav(ele)} >Remove🗑️</button>
+    <div className={styles.bookItem}>
+      <div className={styles.bookInfo}>
+        <h2 className={styles.bookTitle}>{title}</h2>
+        <p className={styles.bookAuthor}>by {author}</p>
+      </div>
+      <div className={styles.bookActions}>
+        <a
+          href={link}
+          style={{ textDecoration: "none" }}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className={styles.readButton}>
+            <Book size={18} />
+            Read
+          </button>
+        </a>
+        <button className={styles.removeButton} onClick={() => Fav(ele)}>
+          <Trash2 size={18} />
+          Remove
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-const mapStateToProps=(state)=>{}
+const mapStateToProps = (state) => {};
 
-const mapDispatchToProps=(dispatch)=>({
-    Fav:(ele)=>dispatch(removeFav(ele))
-})
-export default connect(mapStateToProps,mapDispatchToProps) (List);
-
-
+const mapDispatchToProps = (dispatch) => ({
+  Fav: (ele) => dispatch(removeFav(ele)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(List);
