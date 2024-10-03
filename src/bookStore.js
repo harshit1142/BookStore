b69935942f1d159c005fd9d39e6420ce490003cf
lakshitrajput <lakshit1729@gmail.com>
2024-10-03 15:08:31 +0530

    Added books to src/bookStore.js

--- src/bookStore.js
+++ src/bookStore.js
@@ -17,4 +17,31 @@ export default [
         averageRating:"4.0",
         previewLink:"https://openlibrary.org/works/OL483391W/Charlotte%27s_Web"
     },
+    {
+        id:"3",
+        title:"The dreamsnatcher",
+        subTitle:"",
+        authors: ["Abi Elphinstone"],
+        publishedDate:"26 February, 2015",
+        averageRating:"4.0",
+        previewLink:"https://openlibrary.org/works/OL21485703W/The_dreamsnatcher"
+    },
+    {
+        id:"4",
+        title:"Scott Pilgrim (Comic Series)",
+        subTitle:"",
+        authors: ["Bryan Lee O'Malley"],
+        publishedDate:"14 August, 2004",
+        averageRating:"4.8",
+        previewLink:"https://archive.org/details/6-scott-pilgrim/1%23%20Scott%20Pilgrim%27s%20Precious%20Little%20Life/page/181/mode/2up"
+    },
+    {
+        id:"5",
+        title:"Vedic Mathematics",
+        subTitle:"",
+        authors: ["Bharati Krishna Tirth Ji Maharaj"],
+        publishedDate:"1 December, 1985",
+        averageRating:"5.0",
+        previewLink:"https://archive.org/details/vedic-mathematics-bharati-krishna-tirth-ji-maharaj"
+    },
 ]
