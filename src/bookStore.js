def9b0ad067e39ee5a75765fe063b7f08173727b
Kamp7 <kamleshwarcodes@gmail.com>
2024-10-03 14:43:42 +0530

    add a book

--- src/bookStore.js
+++ src/bookStore.js
@@ -13,8 +13,17 @@ export default [
         title:"Charlotte's Web",
         subTitle:"",
         authors: ["E.B.White"],
-        publishedDate:"May 1, 2002",
+        publishedDate:"October 15, 1952",
         averageRating:"4.0",
         previewLink:"https://openlibrary.org/works/OL483391W/Charlotte%27s_Web"
+    },
+    {
+      id:"3",
+      title:"Eat that frog!",
+        subTitle:"",
+        authors: ["Brian Tracy"],
+        publishedDate:"2001",
+        averageRating:"3.9",
+        previewLink:"https://openlibrary.org/works/OL2392324W/Eat_That_Frog%21_21_Great_Ways_to_Stop_Procrastinating_and_Get_More_Done_in_Less_Time"
     },
 ]
