e41a0442b285abdd7c39849a66e84a411003e0a4
Kamp7 <kamleshwarcodes@gmail.com>
2024-10-03 14:15:19 +0530

    add a book

--- src/bookStore.js
+++ src/bookStore.js
@@ -8,4 +8,13 @@ export default [
         averageRating:"4.5",
         previewLink:"https://openlibrary.org/books/OL8015118M/How_to_Make_Someone_Love_You_Forever%21_In_90_Minutes_or_Less",
     },
+    {
+        id:"2",
+        title:"Charlotte's Web",
+        subTitle:"",
+        authors: ["E.B.White"],
+        publishedDate:"May 1, 2002",
+        averageRating:"4.0",
+        previewLink:"https://openlibrary.org/works/OL483391W/Charlotte%27s_Web"
+    },
 ]
