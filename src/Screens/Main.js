2db762c77f18dd3df74885eeaf11fbd70d5010dd
Chayandev <chayandevbera@gmail.com>
2024-10-06 21:12:27 +0530

    serach-bar-ui-improvements-and-adding-clear-functionality

--- src/Screens/Main.js
+++ src/Screens/Main.js
@@ -2,9 +2,9 @@ import React, { useMemo, useState } from "react";
 import Book from "../Components/Book";
 import { Link } from "react-router-dom";
 import styles from "./BookStore.module.css";
-import { Search, Heart } from "lucide-react";
+import { Search, Heart, XCircle } from "lucide-react";
 import Loading from "../Components/LoadingSpinner";
-import bookStore from '../bookStore'
+import bookStore from "../bookStore";
 
 export default function Main() {
   const [book, setBook] = useState([]);
@@ -17,17 +17,19 @@ export default function Main() {
         `https://www.googleapis.com/books/v1/volumes?q=${search}&download=epub&key=AIzaSyD_d_29Zq6n63LUjWQMIJvVFY2QI7Rwb4E`
       )
         .then((res) => res.json())
-        .then((data) => {console.log(data); setBook(data);})
+        .then((data) => {
+          console.log(data);
+          setBook(data);
+        });
     } catch (error) {
       console.log(error);
     }
   }, [search]);
 
-  function handleSearch(e) {
-    e.preventDefault();
-    text === "" ? setSearch(search) : setSearch(text);
-    setText("");
-  }
+  // Function to clear the input when the cross icon is clicked
+  const clearSearch = () => {
+    setText(""); // Clears the search text
+  };
   return (
     <>
       <div className={styles.container}>
@@ -38,18 +40,27 @@ export default function Main() {
           </h1>
         </header>
         <main className={styles.main}>
-          <form className={styles.searchContainer} onSubmit={handleSearch}>
-            <input
-              type="text"
-              placeholder="Search for books..."
-              className={styles.searchInput}
-              onChange={(e) => setText(e.target.value)}
-            />
-            <button className={styles.searchButton}>
-              <Search size={18}/>
-              Search
-            </button>
-          </form>
+          <div className={styles.searchContainer}>
+            <div className={styles.searchWrapper}>
+              {/* Search Icon */}
+              <Search size={18} className={styles.searchIcon} />
+              <input
+                type="text"
+                value={text}
+                placeholder="Search for books..."
+                className={styles.searchInput}
+                onChange={(e) => setText(e.target.value)}
+              />
+              {/* Conditionally render the Clear (Cross) icon if there is text */}
+              {text && (
+                <XCircle
+                  size={18}
+                  className={styles.clearIcon}
+                  onClick={clearSearch}
+                />
+              )}
+            </div>
+          </div>
           <Link
             to="/favorite"
             style={{ textDecoration: "none" }}
@@ -60,42 +71,43 @@ export default function Main() {
           </Link>
         </main>
         <div className="main">
-        {Object.keys(book).length === 0 ? (
-          <Loading/>
-        ) : (
-          book.items && book.items.map((ele) => (
-            <Book
-              title={ele.volumeInfo?.title}
-              subTitle={ele.volumeInfo?.subtitle}
-              author={ele.volumeInfo?.authors && ele.volumeInfo.authors[0]}
-              date={ele.volumeInfo?.publishedDate}
-              rating={ele.volumeInfo?.averageRating}
-              link={ele.volumeInfo?.previewLink}
-              key={ele.id}
-              ele={ele}
-              thumb={ele.volumeInfo.imageLinks.thumbnail}
-            />
-          ))
-        )}
-      </div>
-      <div className="fav">
-        <h1 className="txt-d"> Contributed Books </h1>
-        <div className="main ">
-          {bookStore.map((ele) => (
-            <Book
-              title={ele.title}
-              subTitle={ele.subtitle}
-              author={ele.authors && ele.authors[0]}
-              date={ele.publishedDate}
-              rating={ele.averageRating}
-              link={ele.previewLink}
-              key={ele.id}
-              ele={ele}
-              thumb={ele.thumnail}
-            />
-          ))}
+          {Object.keys(book).length === 0 ? (
+            <Loading />
+          ) : (
+            book.items &&
+            book.items.map((ele) => (
+              <Book
+                title={ele.volumeInfo?.title}
+                subTitle={ele.volumeInfo?.subtitle}
+                author={ele.volumeInfo?.authors && ele.volumeInfo.authors[0]}
+                date={ele.volumeInfo?.publishedDate}
+                rating={ele.volumeInfo?.averageRating}
+                link={ele.volumeInfo?.previewLink}
+                key={ele.id}
+                ele={ele}
+                thumb={ele.volumeInfo.imageLinks.thumbnail}
+              />
+            ))
+          )}
         </div>
-      </div>
+        <div className="fav">
+          <h1 className="txt-d"> Contributed Books </h1>
+          <div className="main ">
+            {bookStore.map((ele) => (
+              <Book
+                title={ele.title}
+                subTitle={ele.subtitle}
+                author={ele.authors && ele.authors[0]}
+                date={ele.publishedDate}
+                rating={ele.averageRating}
+                link={ele.previewLink}
+                key={ele.id}
+                ele={ele}
+                thumb={ele.thumnail}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
