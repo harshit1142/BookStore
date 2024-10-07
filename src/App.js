da534e65b64d7c04755d14bc2d054d84ca0d4b82
maitri-vv <maitrivaghasiya99@gmail.com>
2024-10-07 12:20:44 +0530

    I have added the back to top button
    I have added the button
    Closes issue : #35

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Main from "./Screens/Main";
 import store from "./redux/store";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DisplayFav from "./Components/DisplayFav";
+import BackToTop from "./Components/BackToTop";
 
 function App() {
   return (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/Favorite" element={<DisplayFav />} />
-      </Routes>
-    </BrowserRouter>
-  </Provider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          {/* Your existing app content */}
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/Favorite" element={<DisplayFav />} />
+          </Routes>
+          <BackToTop /> {/* Add the BackToTop component here */}
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
